a644db4d0601af69e3ed5bada942d60249a4eb7b
Rohn Edwards <rohn.edwards@gmail.com>
2017-07-31 18:24:22 -0500

    Create tests for testing var and cmd polution
    Some work is about to be done to make it so any module(s) created with
    the DBR framework don't see unnecessary helper variables and commands
    that the framework uses.
    
    The test can't pass right now, but it works by taking a snapshot of
    vars and cmds before dot sourcing the framework, then taking another
    snapshot afterwards. From inside the module's scope, only excepted
    variables and commands should be added (the excepted list is empty,
    which is why the test can't pass right now)
    
    Before making changes, here's a list of the vars and cmds that are
    visible inside of a module that uses the framework:
      * Variable: __CommandDeclarations
      * Variable: __CommandsBefore
      * Variable: __DbReaderInfoTableName
      * Variable: __FakeAttributes
      * Variable: __OutputNullReplacementString
      * Variable: BoundDateTimeCompleter
      * Variable: BoundStandardCompleter
      * Variable: ReferenceCommandScriptBlock
      * Variable: StandardArgumentCompleter
      * Variable: TabExpansionAvailable
      * Command: AddDbReaderInfo
      * Command: AsDbConnectionType
      * Command: CombineDbReaderInfo
      * Command: ContainsMatch
      * Command: ConvertToSimpleXml
      * Command: DateTimeConverter
      * Command: DbReaderCommand
      * Command: EvaluateAttributeArgumentValue
      * Command: Get-DbReaderConnection
      * Command: GetInheritedClasses
      * Command: GetModuleSettingsDictionary
      * Command: GetValueFromAst
      * Command: InvokeReaderCommand
      * Command: MergeHelpInfo
      * Command: NewCompletionResult
      * Command: NewDbReaderInfo
      * Command: NewOrderByString
      * Command: New-TableFormatXml
      * Command: ParseParamBlock
      * Command: PrepReaderInfoForSql
      * Command: Set-DbReaderConnection

---
+++ Tests/DatabaseReporter.Framework.tests.ps1
@@ -0,0 +1,33 @@
+Describe 'Framework Health' {
+    It 'Doesn''t pollute its parent module' {
+        # Right now this test has no way of passing. The goal is going to be to minimize the
+        # amount of module scope pollution, but it will never make it down to 0
+
+        $AllowedVariables = @()
+        $AllowedCommands = @()
+
+        $Module = New-Module {
+            $__VariablesBefore = Get-Variable -Scope Script
+            $__CommandsBefore = Get-Command
+
+            . "$PSScriptRoot\..\DatabaseReporter.ps1"
+
+            $__VariablesAfter = Get-Variable -Scope Script | Where-Object Name -notin '__VariablesBefore', '__VariablesAfter'
+            $__CommandsAfter = Get-Command
+
+            Export-ModuleMember -Variable *
+        }
+
+        $NewVariables = Compare-Object $__VariablesBefore $__VariablesAfter -Property Name | 
+            Where-Object SideIndicator -eq '=>' | 
+            Where-Object Name -notin $AllowedVariables |
+            ForEach-Object { 'Variable: {0}' -f $_.Name }
+
+        $NewCommands = Compare-Object $__CommandsBefore $__CommandsAfter -Property Name | 
+            Where-Object SideIndicator -eq '=>' | 
+            Where-Object Name -notin $AllowedCommands | 
+            ForEach-Object { 'Command: {0}' -f $_.Name }
+
+        (@($NewVariables) + $NewCommands) -join "`n" | Should BeNullOrEmpty
+    } 
+}
