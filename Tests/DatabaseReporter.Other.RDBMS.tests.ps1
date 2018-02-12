9856661dad03167925549b7d488cccd72b1d12d1
Rohn Edwards <rohn.edwards@gmail.com>
2018-02-12 17:57:42 -0600

    Add test for WildcardReplacementScriptblock functionality

--- Tests/DatabaseReporter.Other.RDBMS.tests.ps1
+++ Tests/DatabaseReporter.Other.RDBMS.tests.ps1
@@ -28,9 +28,35 @@ Describe 'LIKE supports ESCAPE clause (Tested for SQLite)' {
             }
         } 
 
-        $Expected = "SELECT Customers.CustomerID as CustomerID*Customers.Title AS Title FROM Customers JOIN Orders on Customers.CustomerId = Orders.CustomerId*WHERE*Customers.LastName LIKE @LastName0 ESCAPE '\'" | NormalizeQuery
+        $Expected = "SELECT Customers.CustomerID as CustomerID*Customers.Title AS Title FROM Customers JOIN Orders on Customers.CustomerId = Orders.CustomerId*WHERE*Customers.LastName LIKE @LastName0 ESCAPE '\'*" | NormalizeQuery
         $Result = Get-CustomerWildcardEscape -LastName t%est_* -ReturnSqlQueryNew
         $Result.Query | NormalizeQuery | Should BeLike $Expected
         $Result.Parameters['@LastName0'] | Should Be 't\%est\_%'
+    }
+
+    It 'WildcardReplacementScriptblock allows custom logic' {
+        $TestMod = New-Module -Name DBTest -ScriptBlock {
+            $DebugMode = $true
+
+            . "$PSScriptRoot\..\DatabaseReporter.ps1"
+
+            DbReaderCommand Get-TestData {
+                [MagicDbInfo(
+                    FromClause='Test',
+                    DbConnectionString = 'FakeConnectionString',
+                    DbConnectionType = 'System.Data.SqlClient.SqlConnection',
+                    WildcardReplacementScriptblock={ ($_ -replace '(?<!\\)@', '%') -replace '\\@', '@'}
+                )]
+                param(
+                    [MagicDbProp()]
+                    [string] $Email
+                )
+            }
+        }
+
+        $Expected = "SELECT Email AS Email FROM Test*WHERE*Email LIKE @Email0*" | NormalizeQuery
+        $Result = Get-TestData -Email test\@@.com -ReturnSqlQueryNew
+        $Result.Query | NormalizeQuery | Should BeLike $Expected
+        $Result.Parameters['@Email0'] | Should Be 'test@%.com'
     }
 }
