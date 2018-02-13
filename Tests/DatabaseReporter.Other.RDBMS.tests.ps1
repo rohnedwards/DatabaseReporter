55f36197019856894d707356ee04e82bd2ca5327
Rohn Edwards <rohn.edwards@gmail.com>
2018-02-13 09:54:03 -0600

    Add tests for new MagicDbInfo() properties
    The SQLite escaping test was modified to test functionality when SqlMode
    is hardcoded to 'SQLite', and a new, non-functional test was added to
    test the functionality when the framework autodetects SQLite mode (that
    one doesn't do anything yet because we can't use a SQLite DB connection
    type without the assembly being loaded)
    
    Tests were also added for an invalid SqlMode, which should simply be
    ignored and behave as if there were no SqlMode defined, and for the
    WhereConditionScriptblock being defined.

--- Tests/DatabaseReporter.Other.RDBMS.tests.ps1
+++ Tests/DatabaseReporter.Other.RDBMS.tests.ps1
@@ -2,7 +2,7 @@
 
 Describe 'LIKE supports ESCAPE clause (Tested for SQLite)' {
 
-    It '_ and % are properly escaped' {
+    It '_ and % are properly escaped in SqlLite mode (SqlMode used)' {
 
         $TestMod = New-Module -Name DBTest -ScriptBlock {
             $DebugMode = $true
@@ -13,7 +13,8 @@ Describe 'LIKE supports ESCAPE clause (Tested for SQLite)' {
                 [MagicDbInfo(
                     FromClause = 'Customers JOIN Orders on Customers.CustomerId = Orders.CustomerId',
                     DbConnectionString = 'FakeConnectionString',
-                    DbConnectionType = 'System.Data.SqlClient.SqlConnection'
+                    DbConnectionType = 'System.Data.SqlClient.SqlConnection',
+                    SqlMode='SQLite'
                 )]
                 param(
                     [MagicDbProp(ColumnName='Customers.CustomerId')]
@@ -32,6 +33,12 @@ Describe 'LIKE supports ESCAPE clause (Tested for SQLite)' {
         $Result = Get-CustomerWildcardEscape -LastName t%est_* -ReturnSqlQueryNew
         $Result.Query | NormalizeQuery | Should BeLike $Expected
         $Result.Parameters['@LastName0'] | Should Be 't\%est\_%'
+    }
+
+    It '_ and % are properly escaped in SQLite mode (Auto detect)' {
+        # No test yet since the framework won't allow an unknown type to be used.
+        # Easy enough to test, but easiest way requires adding Sqlite library with
+        # call to Add-Type...
     }
 
     It 'WildcardReplacementScriptblock allows custom logic' {
@@ -58,5 +65,71 @@ Describe 'LIKE supports ESCAPE clause (Tested for SQLite)' {
         $Result = Get-TestData -Email test\@@.com -ReturnSqlQueryNew
         $Result.Query | NormalizeQuery | Should BeLike $Expected
         $Result.Parameters['@Email0'] | Should Be 'test@%.com'
+    }
+
+    It 'WhereConditionBuilder allows custom logic' {
+        $TestMod = New-Module -Name DBTest -ScriptBlock {
+            $DebugMode = $true
+
+            . "$PSScriptRoot\..\DatabaseReporter.ps1"
+
+            DbReaderCommand Get-TestData {
+                [MagicDbInfo(
+                    FromClause='Test',
+                    DbConnectionString = 'FakeConnectionString',
+                    DbConnectionType = 'System.Data.SqlClient.SqlConnection',
+                    WhereConditionBuilderScriptblock={ 
+                        if ($args[1] -eq 'LIKE') {
+                            '{0} {1} {2} /* This is a {1} test */' -f $args 
+                        }
+                        else {
+                            '{0} {1} {2}' -f $args
+                        }
+                    }
+                )]
+                param(
+                    [MagicDbProp()]
+                    [string] $Email
+                )
+            }
+        }
+
+        $ExpectedWithLike = "SELECT Email AS Email FROM Test*WHERE*Email LIKE @Email0*This is a LIKE test*" | NormalizeQuery
+        $ExpectedWithEq = "SELECT Email AS Email FROM Test*WHERE*Email = @Email0*" | NormalizeQuery
+        $Result = Get-TestData -Email test -ReturnSqlQueryNew
+        $Result.Query | NormalizeQuery | Should BeLike $ExpectedWithLike
+
+        $Result = Get-TestData -Email @{V='test'; ComparisonOperator='='} -ReturnSqlQueryNew
+        $Result.Query | NormalizeQuery | Should BeLike $ExpectedWithEq
+        $Result.Query | NormalizeQuery | Should Not BeLike '*This is a * test'
+    }
+
+    It 'Invalid SqlMode doesn''t cause warnings or errors' {
+        $TestMod = New-Module -Name DBTest -ScriptBlock {
+            $DebugMode = $true
+
+            . "$PSScriptRoot\..\DatabaseReporter.ps1"
+
+            DbReaderCommand Get-TestData {
+                [MagicDbInfo(
+                    FromClause='Test',
+                    DbConnectionString = 'FakeConnectionString',
+                    DbConnectionType = 'System.Data.SqlClient.SqlConnection',
+                    SqlMode='ThisShouldNeverBeValid'
+                )]
+                param(
+                    [MagicDbProp()]
+                    [string] $Email
+                )
+            }
+        } -WarningAction SilentlyContinue -ErrorAction SilentlyContinue -WarningVariable TestWarnings -ErrorVariable TestErrors
+
+        $TestWarnings.Count | Should Be 0
+        $TestErrors.Count | Should Be 0
+
+        $Expected = "SELECT Email AS Email FROM Test*WHERE*Email LIKE @Email0*" | NormalizeQuery
+        $Result = Get-TestData -Email test -ReturnSqlQueryNew
+        $Result.Query | NormalizeQuery | Should BeLike $Expected
+        $Result.Parameters['@Email0'] | Should Be 'test'
     }
 }
